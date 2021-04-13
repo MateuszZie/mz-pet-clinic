ed45188eb9aaed7d142b4b64810c41684c8f32e6
Mateusz Ziebura <mateusz_860@op.pl>
2021-04-13 11:05:05 +0200

    Initial project add

---
+++ src/main/java/org/mateuszziebura/mzpetclinic/MzPetClinicApplication.java
@@ -0,0 +1,13 @@
+package org.mateuszziebura.mzpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MzPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mateuszziebura/mzpetclinic/MzPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mateuszziebura.mzpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MzPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
