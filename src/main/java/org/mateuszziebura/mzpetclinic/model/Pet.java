b9400def055a0cee9996ab87564c5fd47e4f70b6
Mateusz Ziebura <mateusz_860@op.pl>
2021-04-14 12:53:56 +0200

    adding POJOs. Closes #2.

---
+++ src/main/java/org/mateuszziebura/mzpetclinic/model/Pet.java
@@ -0,0 +1,13 @@
+package org.mateuszziebura.mzpetclinic.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+}
---
+++ src/main/java/org/mateuszziebura/mzpetclinic/model/PetType.java
@@ -0,0 +1,9 @@
+package org.mateuszziebura.mzpetclinic.model;
+
+import lombok.Data;
+
+@Data
+public class PetType {
+
+    private String name;
+}
