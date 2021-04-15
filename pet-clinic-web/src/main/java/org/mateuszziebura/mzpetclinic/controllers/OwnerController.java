6fd909d63c3ccc96194c9854b611fdbfd6d6854d
Mateusz Ziebura <mateusz_860@op.pl>
2021-04-15 15:48:43 +0200

    Create Owner index page and controller closes #13

---
+++ pet-clinic-web/src/main/java/org/mateuszziebura/mzpetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package org.mateuszziebura.mzpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/","","/index","/index.html"})
+    public String ownerList(){
+
+        return "owners/index";
+    }
+}
