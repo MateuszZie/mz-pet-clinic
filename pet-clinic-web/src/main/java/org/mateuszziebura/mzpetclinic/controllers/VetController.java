c9dc68a0440fa7d6832718622e3fd91d5872df0b
Mateusz Ziebura <mateusz_860@op.pl>
2021-04-16 15:58:41 +0200

    List all Vets on Vet index page closes #15

--- pet-clinic-web/src/main/java/org/mateuszziebura/mzpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/mateuszziebura/mzpetclinic/controllers/VetController.java
@@ -1,14 +1,22 @@
 package org.mateuszziebura.mzpetclinic.controllers;
 
+import org.mateuszziebura.mzpetclinic.services.VetServices;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index","/vest/index.html"})
-    public String vetList(){
+    private final VetServices vetServices;
 
+    public VetController(VetServices vetServices) {
+        this.vetServices = vetServices;
+    }
+
+    @RequestMapping({"/vets","/vets/index","/vest/index.html"})
+    public String vetList(Model model){
+        model.addAttribute("vets", vetServices.findAll());
         return "vets/index";
     }
 }
