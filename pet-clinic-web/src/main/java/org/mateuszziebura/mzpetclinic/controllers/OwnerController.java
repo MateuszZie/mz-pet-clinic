307c51ef260cc3cefd9237b09e756ec4b25b1279
Mateusz Ziebura <mateusz_860@op.pl>
2021-04-16 15:46:56 +0200

    List all Owners on Owner index page closes #16

--- pet-clinic-web/src/main/java/org/mateuszziebura/mzpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/org/mateuszziebura/mzpetclinic/controllers/OwnerController.java
@@ -1,15 +1,23 @@
 package org.mateuszziebura.mzpetclinic.controllers;
 
+import org.mateuszziebura.mzpetclinic.services.OwnerServices;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"/","","/index","/index.html"})
-    public String ownerList(){
+    private final OwnerServices ownerServices;
 
+    public OwnerController(OwnerServices ownerServices) {
+        this.ownerServices = ownerServices;
+    }
+
+    @RequestMapping({"/","","/index","/index.html"})
+    public String ownerList(Model model){
+        model.addAttribute("owners",ownerServices.findAll());
         return "owners/index";
     }
 }
