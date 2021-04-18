3c799e06e9053cdb4e939bd7e54ff363a18b702f
Mateusz Ziebura <mateusz_860@op.pl>
2021-04-18 18:23:16 +0200

    Create Specialty Map Service Impl closes #31

---
+++ pet-clinic-data/src/main/java/org/mateuszziebura/mzpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package org.mateuszziebura.mzpetclinic.services;
+
+import org.mateuszziebura.mzpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
