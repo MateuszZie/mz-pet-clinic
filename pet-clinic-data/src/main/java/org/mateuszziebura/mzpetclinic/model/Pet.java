58795d75a68128dc0e2352e5cd29f60616226eef
Mateusz Ziebura <mateusz_860@op.pl>
2021-04-22 13:48:57 +0200

    Convert Owners to JPA Entities closes #36

--- pet-clinic-data/src/main/java/org/mateuszziebura/mzpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/org/mateuszziebura/mzpetclinic/model/Pet.java
@@ -3,14 +3,26 @@ package org.mateuszziebura.mzpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "pet")
 public class Pet extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "types")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner")
     private Owner owner;
+
+    @Column(name = "birth_day")
     private LocalDate birthDate;
 }
