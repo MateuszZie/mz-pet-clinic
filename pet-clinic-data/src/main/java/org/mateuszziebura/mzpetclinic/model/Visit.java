c7b84efd33e41880b727b7dea763436867904717
Mateusz Ziebura <mateusz_860@op.pl>
2021-04-22 14:36:09 +0200

    Create Visit Entity closes #38

--- pet-clinic-data/src/main/java/org/mateuszziebura/mzpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/org/mateuszziebura/mzpetclinic/model/Visit.java
@@ -3,13 +3,21 @@ package org.mateuszziebura.mzpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
 
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet")
     private Pet pet;
 }
