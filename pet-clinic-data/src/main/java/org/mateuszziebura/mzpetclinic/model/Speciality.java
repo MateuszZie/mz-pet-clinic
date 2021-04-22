796acdc48fb2bf403f5c53616c7e63f9aa00ea6b
Mateusz Ziebura <mateusz_860@op.pl>
2021-04-22 14:11:10 +0200

    Convert Vets to JPA Entities closes #37

--- pet-clinic-data/src/main/java/org/mateuszziebura/mzpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/org/mateuszziebura/mzpetclinic/model/Speciality.java
@@ -3,9 +3,16 @@ package org.mateuszziebura.mzpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 }
--- pet-clinic-data/src/main/java/org/mateuszziebura/mzpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org/mateuszziebura/mzpetclinic/model/Vet.java
@@ -3,12 +3,19 @@ package org.mateuszziebura.mzpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+
 @Getter
 @Setter
+@Entity
+@Table(name = "vet")
 public class Vet extends Person{
 
+    @ManyToMany
+    @JoinTable(name = "vet_speciality", joinColumns = @JoinColumn(name = "vet_id"),
+    inverseJoinColumns = @JoinColumn(name = "speciality"))
     private Set<Speciality> specialities = new HashSet<>();
 }
